c03def724bd3449e9749e5975115736a8f270fd0
88thman <gillichharry@gmx.de>
2021-11-03 22:12:02 +0100

    a

--- LiN/main.js
+++ LiN/main.js
@@ -10,6 +10,7 @@ var midi = null;  // global MIDIAccess object
 function onMIDISuccess( midiAccess ) {
   console.log( "MIDI ready!" );
   midi = midiAccess;  // store in the global (in real usage, would probably keep in an object instance)
+  listInputsAndOutputs(midiAccess);
 }
 
 function onMIDIFailure(msg) {
@@ -17,3 +18,19 @@ function onMIDIFailure(msg) {
 }
 
 navigator.requestMIDIAccess().then( onMIDISuccess, onMIDIFailure );
+
+function listInputsAndOutputs( midiAccess ) {
+  for (var entry of midiAccess.inputs) {
+    var input = entry[1];
+    console.log( "Input port [type:'" + input.type + "'] id:'" + input.id +
+      "' manufacturer:'" + input.manufacturer + "' name:'" + input.name +
+      "' version:'" + input.version + "'" );
+  }
+
+  for (var entry of midiAccess.outputs) {
+    var output = entry[1];
+    console.log( "Output port [type:'" + output.type + "'] id:'" + output.id +
+      "' manufacturer:'" + output.manufacturer + "' name:'" + output.name +
+      "' version:'" + output.version + "'" );
+  }
+}
