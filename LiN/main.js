1c73a577943f3a153d43ba4181a3f7b302e4682e
88thman <gillichharry@gmx.de>
2021-11-03 22:07:06 +0100

    a

--- LiN/main.js
+++ LiN/main.js
@@ -5,3 +5,15 @@ LiN.load = function() {
 };
 
 window.addEventListener('load', LiN.load);
+
+var midi = null;  // global MIDIAccess object
+function onMIDISuccess( midiAccess ) {
+  console.log( "MIDI ready!" );
+  midi = midiAccess;  // store in the global (in real usage, would probably keep in an object instance)
+}
+
+function onMIDIFailure(msg) {
+  console.log( "Failed to get MIDI access - " + msg );
+}
+
+navigator.requestMIDIAccess().then( onMIDISuccess, onMIDIFailure );
