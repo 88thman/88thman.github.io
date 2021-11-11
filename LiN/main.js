119178092486c17b48f5cc5a7348b311ed0bc398
88thman <gillichharry@gmx.de>
2021-11-11 14:17:59 +0100

    a

--- LiN/main.js
+++ LiN/main.js
@@ -6,7 +6,11 @@ function include(fileName) {
 }
 
 
-function LiN() { }
+function LiN() {
+	this.load = function() {
+		include("midi.js");
+	}
+}
 window.addEventListener('load', LiN.load);
 
 LiN.prototype.updateInputs = function (e) {
@@ -26,22 +30,3 @@ LiN.prototype.updateInputs = function (e) {
 }
 
 
-
-/*
-function listInputsAndOutputs( midiAccess ) {
-	console.log( "List of Ports:" );
-	for (var entry of midiAccess.inputs) {
-		var input = entry[1];
-		console.log( "Input port [type:'" + input.type + "'] id:'" + input.id +
-			"' manufacturer:'" + input.manufacturer + "' name:'" + input.name +
-			"' version:'" + input.version + "'" );
-	}
-
-	for (var entry of midiAccess.outputs) {
-		var output = entry[1];
-		console.log( "Output port [type:'" + output.type + "'] id:'" + output.id +
-			"' manufacturer:'" + output.manufacturer + "' name:'" + output.name +
-			"' version:'" + output.version + "'" );
-	}
-}
-*/
--- LiN/midi.js
+++ LiN/midi.js
@@ -13,3 +13,23 @@ Midi.prototype.setInput = function (inputIndex) {
 
 let midi = new Midi();
 navigator.requestMIDIAccess().then(Midi.load, () => { });
+
+
+/*
+function listInputsAndOutputs( midiAccess ) {
+	console.log( "List of Ports:" );
+	for (var entry of midiAccess.inputs) {
+		var input = entry[1];
+		console.log( "Input port [type:'" + input.type + "'] id:'" + input.id +
+			"' manufacturer:'" + input.manufacturer + "' name:'" + input.name +
+			"' version:'" + input.version + "'" );
+	}
+
+	for (var entry of midiAccess.outputs) {
+		var output = entry[1];
+		console.log( "Output port [type:'" + output.type + "'] id:'" + output.id +
+			"' manufacturer:'" + output.manufacturer + "' name:'" + output.name +
+			"' version:'" + output.version + "'" );
+	}
+}
+*/
